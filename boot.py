98a2779bf6d9c3a1659361aa079b251d41b4a121
Adamsky47 <55794259+Adamsky47@users.noreply.github.com>
2022-10-03 22:43:33 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,4 @@
-# The Pico will check this file first to configure the usb settings
+# The Pico will check this file first to configure the serial communication settings
 
 import usb_cdc
 usb_cdc.enable(console=True, data=True)
