0308ec544386a8116627ef5f8ac5a7bdc66eecd5
Adamsky47 <55794259+Adamsky47@users.noreply.github.com>
2022-10-03 22:10:30 +0200

    Create code.py
    This is the main code that the Raspberry Pi Pico will run when plugged into a power source.

---
+++ code.py
@@ -0,0 +1,44 @@
+# Importing required modules
+from time import sleep
+import board
+import busio
+import digitalio
+from digitalio import DigitalInOut
+from adafruit_mcp2515.canio import Message, RemoteTransmissionRequest
+from adafruit_mcp2515 import MCP2515 as CAN
+import usb_cdc
+
+
+# Define the required pins for the CAN HAT
+cs = DigitalInOut(board.GP17)
+cs.switch_to_output()
+spi = busio.SPI(board.GP18, board.GP19, board.GP16)
+
+# Initialise the CAN transiever on the pico and set it to accept external data
+can_bus = CAN(spi, cs, loopback=False, silent=True)
+c = 0
+x = True
+
+led = digitalio.DigitalInOut(board.GP28)
+led.direction = digitalio.Direction.OUTPUT
+
+usb_device = usb_cdc.data
+
+# Set the receiver to listening and display the messages and sender ID
+while True:
+    with can_bus.listen(timeout=1.0) as listener:
+        message_count = listener.in_waiting()
+        for _i in range(message_count):
+            msg = listener.receive()
+            print("Message from ", hex(msg.id))
+            if isinstance(msg, Message):
+                print("Message data:", msg.data)
+                led.value = True
+                sleep(0.5)
+                led.value = False
+                sleep(1)
+                x = True
+                while x == True:
+                    usb_device.write(msg.data)
+                    sleep(1)
+                    x = False
